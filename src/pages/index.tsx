9012c2fefebdffa1ae891f0083c6b7aebdf30f62
stevenmqnguyen <stevenmqnguyen@gmail.com>
2023-03-17 03:08:28 -0500

    Add style to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex h-screen flex-col justify-between">
+    <div className="flex h-screen flex-col justify-between bg-black">
       <Head>
         <title>lttstore.com feed</title>
         <meta
@@ -15,23 +15,44 @@ const Home: NextPage = () => {
       </Head>
       <div />
       <main className="flex flex-col items-center justify-center">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-6xl font-bold uppercase">lttstore.com Feed</h1>
-          <a
-            href="https://lttstorefeed.vercel.app/api/feed"
-            className="uppercase underline"
-          >
+        {/* <div className="container flex flex-col items-center"> */}
+        <div className="flex flex-col  gap-5 bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-teal-400 via-fuchsia-600 to-rose-600 bg-clip-text text-transparent">
+          <h1 className="text-6xl font-extrabold uppercase">
+            <a href="https://lttstore.com">lttstore.com</a> Feed
+          </h1>
+          {/* <p className="bg-gradient-to-l from-teal-800 via-fuchsia-500 to-rose-600 bg-clip-text text-xl font-extrabold uppercase text-transparent">
             All Products Feed
+          </p> */}
+          <a href="https://lttstorefeed.vercel.app/api/feed">
+            <p className="text-xl font-bold uppercase text-transparent">
+              All Products Feed
+            </p>
+          </a>
+          <a href="https://lttstorefeed.vercel.app/api/feed">
+            <p className="text-xl font-extrabold uppercase text-transparent">
+              Gear Products Feed
+            </p>
+          </a>
+          <a href="https://lttstorefeed.vercel.app/api/feed">
+            <p className="text-xl font-extrabold uppercase text-transparent">
+              Clothing Products Feed
+            </p>
+          </a>
+          <a href="https://lttstorefeed.vercel.app/api/feed">
+            <p className="text-xl font-extrabold uppercase text-transparent">
+              The Newsletter Feed
+            </p>
           </a>
         </div>
+        {/* </div> */}
       </main>
       <footer className=" flex flex-col items-center justify-center">
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center text-xs text-white">
           <p>Made with ❤️</p>
           <p>
-            This app and RSS feed is not affiliated with or endorsed by Linus
-            Media Group Inc., Creator Warehouse Inc., or Floatplane Inc. All
-            content and trademarks belong to their respective owners.
+            This app is not affiliated with or endorsed by Linus Media Group
+            Inc., Creator Warehouse Inc., or Floatplane Inc. All content and
+            trademarks belong to their respective owners.
           </p>
         </div>
       </footer>
